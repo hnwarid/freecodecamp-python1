a656332b4a4d4b68b5612cac0f184fda87000925
Hamzah Nurwarid <hn.warid@gmail.com>
2021-12-20 21:33:08 +0700

    defining chart function

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -5,8 +5,8 @@ class Category:
         self.balance = 0
 
     def deposit(self, amount, description=""):
-        self.ledger.append({"amount": amount, "description": description})
         self.balance += amount
+        self.ledger.append({"amount": amount, "description": description})
 
     def get_balance(self):
         return self.balance
@@ -17,8 +17,8 @@ class Category:
     def withdraw(self, amount, description=""):
         if self.balance < amount:
             return False
-        self.ledger.append({"amount": -amount, "description": description})
         self.balance -= amount
+        self.ledger.append({"amount": -amount, "description": description})
         return True
 
     def transfer(self, amount, other_cat):
@@ -31,9 +31,9 @@ class Category:
     def __str__(self):
         string_repr = [self.name.center(30, "*")]
 
-        for dict in self.ledger:
-            desc_line = dict["description"][0:23]
-            string_repr.append("{:<23}{:>7.2f}".format(desc_line, dict["amount"]))
+        for ledger in self.ledger:
+            desc_line = ledger["description"][0:23]
+            string_repr.append("{:<23}{:>7.2f}".format(desc_line, ledger["amount"]))
 
         string_repr.append("Total: {}".format(self.balance))
 
@@ -41,4 +41,19 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
+    category_names = []
+    spending_list = []
+    spending_percentage = []
+    # create spending percentage from category in categories
+    for category in categories:
+        category_names.append(category.name)
+
+        for item in category.ledger:
+            if item["amount"] < 0:
+                spending_list.append(item["amount"])
+    print(f"Spending: {spending_list} with categories as follows: {category_names}")
+    print(f"Category total: {sum(spending_list)} with the list as follows:\n {spending_list}")
+
+    # create the chart from spending percentage
+
+    # create the title of budget name
