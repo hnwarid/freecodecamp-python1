3d97dff722ec8a54a222ada6ef86dffc01a030a7
Hamzah Nurwarid <hn.warid@gmail.com>
2021-12-21 19:47:12 +0700

    Update code

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -4,14 +4,34 @@ import random
 
 class Hat:
     def __init__(self, **kwargs):
-        for key in kwargs:
-            setattr(self, key, kwargs[key])
+        self.contents = []
+        for key in kwargs.keys():
+            for value in range(kwargs[key]):
+                self.contents.append(key)
+        self.balls_dict = kwargs
 
-    def name(self):
+
+    def draw(self):
         pass
+
+    def __str__(self):
+        string_repr = ",".join("{}={}".format(k, v) for k, v in self.balls_dict.items())
+        # string_repr = []
+        # for key, value in self.kwargs.items():
+        #
+        #
+        # string_repr = " ".join(string_repr)
+        return string_repr
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
+    hats = Hat(hat)
+    expected = expected_balls
+    m = num_balls_drawn
+    n = num_experiments
+    print(hats, expected, m, n)
 
 if __name__ == "__main__":
-    pass
+    hat1 = Hat(blue=1, red=3)
+    print(hat1)
